8aee714a9e466e6de2cd426fb7ad454dac2f4927
Samuel L S Araujo <147205495+SamuelloranD@users.noreply.github.com>
2024-02-20 01:13:09 -0300

    007.py

--- 007.py
+++ 007.py
@@ -5,4 +5,5 @@ disciplina = input('Digite o nome da disciplina: ')
 n1 = float(input('Primeira nota: '))
 n2 = float(input('Segunda nota: '))
 s = n1 + n2
+
 print(f'Olá, {nome}, sua média na disciplina de {disciplina} foi de {s/2}.')
